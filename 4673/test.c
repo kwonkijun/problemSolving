34e028ba61375f8f0b3984a4a0ef881945951e7b
Ted <kkj6369@naver.com>
2016-08-27 20:43:55 +0900

    Update test.c
    change file

--- 4673/test.c
+++ 4673/test.c
@@ -1,34 +1,38 @@
 #include<stdio.h>
-#include<string.h>
 
-#define MAX_SIZE 10000
+int self [10001] = {0};
 
-int state [MAX_SIZE];
-
-void selfNumber (int n){
-	int nextNum = 0 ; 
+void d(int x){
+	int i;
+	int next = x;
 	
-	if(n/10000 > 0){
-		
-	}else if(n/1000 >0){
-		
-	}else if(n/100 >0){
-		nextNum = n + n/100 + (n - (n/100) * 100)/10 + n%10;
-	}else if(n/10 >0){
-		nextNum = n + n/10 + n%10;
-	}else{
-		nextNum = n + n;
+	for(i=x; i>0; i/=10){
+		next += i%10;
 	}
-	state[nextNum] = 1;
-	selfNumber(nextNum);
+	if(next <=10000){
+		self[next] = 1;
+		d(next);	
+	} 
 }
 
 int main(){
-	int i=0;
-	selfNumber(1);
+	int i;
 	
-	for(i=0 ; i < 100 ; i++){
-		printf("%d", state[i]);
+	for(i=1; i<=10000;i++){
+		
+		if(self[i]==0){
+			d(i);			
+		}else{
+			//체 거르기	
+		}
+		
+	}
+
+	for(i=1; i<=10000;i++){
+		if(self[i]==0){
+			printf("%d\n", i);
+		}
 	}	
+	
 	return 0;
 }
